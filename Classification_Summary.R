6231d9e0bf7e29a557ddc6790f65aab90d7e636a
Ruidong Li <rli012@ucr.edu>
2020-01-05 01:01:11 -0800

    Update Classification_Summary.R

--- Classification_Summary.R
+++ Classification_Summary.R
@@ -15,6 +15,17 @@ for (model in models) {
       }
       pred.test <- read.csv(fl, row.names=1)
       #print(table(pred=pred.test$ypred, true=pred.test$yobs))
+      
+      a <- sum(pred.test$yobs=='E1' & pred.test$ypred=='E1')
+      b <- sum(pred.test$yobs=='E0' & pred.test$ypred=='E1')
+      c <- sum(pred.test$yobs=='E1' & pred.test$ypred=='E0')
+      d <- sum(pred.test$yobs=='E0' & pred.test$ypred=='E0')
+      
+      sensitivity <- a/(a+c)
+      specificity <- d/(b+d)
+      
+      accuracy <- (a+d)/(a+b+c+d)
+      
       roc.test <- roc(pred.test$yobs, pred.test$yprob, plot=FALSE, ci=TRUE, auc=TRUE)
       auc <- round(roc.test$ci[2],3)
       auc.ci.lower95 <- round(roc.test$ci[1],3)
@@ -22,7 +33,9 @@ for (model in models) {
       
       print (paste0(auc, ' (', auc.ci.lower95, '-', auc.ci.upper95, ')'))
       
-      sumpred <- rbind(sumpred, c(model, dataset, signature.name, auc, auc.ci.lower95, auc.ci.upper95))
+      sumpred <- rbind(sumpred, c(model, dataset, signature.name, 
+                                  accuracy, sensitivity, specificity,
+                                  auc, auc.ci.lower95, auc.ci.upper95))
       
     }
     
@@ -37,6 +50,17 @@ for (model in models) {
       
       pred.test <- read.csv(fl, row.names=1)
       #print(table(pred=pred.test$ypred, true=pred.test$yobs))
+      
+      a <- sum(pred.test$yobs=='E1' & pred.test$ypred=='E1')
+      b <- sum(pred.test$yobs=='E0' & pred.test$ypred=='E1')
+      c <- sum(pred.test$yobs=='E1' & pred.test$ypred=='E0')
+      d <- sum(pred.test$yobs=='E0' & pred.test$ypred=='E0')
+      
+      sensitivity <- a/(a+c)
+      specificity <- d/(b+d)
+      
+      accuracy <- (a+d)/(a+b+c+d)
+      
       roc.test <- roc(pred.test$yobs, pred.test$yprob, plot=FALSE, ci=TRUE, auc=TRUE)
       auc <- round(roc.test$ci[2],3)
       auc.ci.lower95 <- round(roc.test$ci[1],3)
@@ -44,7 +68,9 @@ for (model in models) {
       
       print (paste0(auc, ' (', auc.ci.lower95, '-', auc.ci.upper95, ')'))
       
-      sumpred <- rbind(sumpred, c(model, dataset, signature.name, auc, auc.ci.lower95, auc.ci.upper95))
+      sumpred <- rbind(sumpred, c(model, dataset, signature.name, 
+                                  accuracy, sensitivity, specificity,
+                                  auc, auc.ci.lower95, auc.ci.upper95))
       
     }
     
@@ -53,7 +79,9 @@ for (model in models) {
 }
 
 sumpred <- data.frame(sumpred, stringsAsFactors = F)
-colnames(sumpred) <- c('model','dataset','signature','auc','auc.lower95','auc.upper95')
-sumpred$auc <- as.numeric(sumpred$auc)
-saveRDS(sumpred, 'report/sumpred_for_test.RDS')
+colnames(sumpred) <- c('model','dataset','signature',
+                       'accuracy','sensitivity','specificity',
+                       'auc','auc.lower95','auc.upper95')
 
+sumpred[,4:9] <- apply(sumpred[,4:9], 2, as.numeric)
+saveRDS(sumpred, 'report/sumpred_for_test.RDS')
